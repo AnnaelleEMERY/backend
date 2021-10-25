f0f08a660fa0ad9f35a9069c93a5628f1be25b73
Annaelle <annaelleemery512@gmail.com>
2021-10-25 17:49:26 +0200

    Auto stash before merge of "master" and "origin/master"
    Nouvelle BASE temporaire (user OK)

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser'); //pour rendre le corps de la requete en objet js utilisable 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose');// base de donnée
 
-const userRoutes = require('./routes/user');
+const userRoutes = require('./routes/user'); //accès aux routes user
+// const saucesRoutes = require('./routes/sauces'); //accès aux routes des sauces
 
-
-// Connexion à MongoDB
+//connexion à la base de donnée
 mongoose.connect('mongodb+srv://AnnaE:CoquilleJ56!!@cluster0.eqcby.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     {
         useNewUrlParser: true,
@@ -14,6 +14,7 @@ mongoose.connect('mongodb+srv://AnnaE:CoquilleJ56!!@cluster0.eqcby.mongodb.net/m
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+// activer express
 const app = express();
 
 //Ajout des CORS dans le header
@@ -58,6 +59,49 @@ app.use('/api/sauces', (req, res, next) => {
     res.status(200).json(sauces);
   });
 
-  app.use('/api/auth', userRoutes);
+// post => poster une sauce
+app.post('/api/sauce', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({ message: 'Objet créé !' });
+});
 
+// sauces de base dans le site (temporaire)
+app.use('/api/sauces', (req, res, next) => {
+    const sauces = [
+        {
+            _id: 'oeihfzeoi',
+            name: 'Mon premier objet',
+            manufacturer: 'fabriquant',
+            description: 'Les infos de mon premier objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            mainPepper: 'red pepper',
+            heat: 9,
+            likes: 2,
+            dislikes: 1,
+            usersLiked: ['qsomihvqiosfd', 'qsomihfdcosfd'],
+            usersDisliked: ['qsomihvqfkspo'],
+            userId: 'qsomihvqios',
+        },
+        {
+            _id: 'oeihfzfdgfdfeoi',
+            name: 'Mon deuxième objet',
+            manufacturer: 'fabriquant',
+            description: 'Les infos de mon premier objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            mainPepper: 'pink pepper',
+            heat: 6,
+            likes: 2,
+            dislikes: 1,
+            usersLiked: ['qsomihvqferfd', 'qsomihvgcosfd'],
+            usersDisliked: ['qsymihvqfkspo'],
+            userId: 'qsomihvbios',
+        },
+    ];
+    res.status(200).json(sauces);
+});
+
+// app.use('/api/sauces', saucesRoutes); // appliquer les routes des sauces
+app.use('/api/auth', userRoutes); // appliquer les routes user
+
+//exportation de app.js
 module.exports = app;
