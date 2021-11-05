ac4656c6bce3033826e6464a5a1b5b4c84c4f0f5
Annaelle <annaelleemery512@gmail.com>
2021-11-05 21:41:55 +0100

    finished

--- routes/sauces.js
+++ routes/sauces.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+const sauceCtrl = require('../controllers/sauces');
 const auth = require('../middleware/auth');
+const likesCtrl = require('../controllers/likes');
 const multer = require('../middleware/multer-config');
 
-const sauceCtrl = require('../controllers/sauces');
 
 router.get('/', auth, sauceCtrl.getAllSauces);
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
+
+//Add and remove sauces review
+router.post('/:id/like',auth, likesCtrl.addLikeOrDislike);
 
 module.exports = router;
